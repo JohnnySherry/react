1c65f09e46efe28c03abe3298201f98ff1b559ea
unknown <89635585@qq.com>
2018-12-12 23:01:55 +0800

    table

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,26 +1,95 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Table } from 'antd';
+import reqwest from 'reqwest';
 
-class App extends Component {
+const columns = [{
+  title: 'Name',
+  dataIndex: 'name',
+  sorter: true,
+  render: name => `${name.first} ${name.last}`,
+  width: '20%',
+}, {
+  title: 'Gender',
+  dataIndex: 'gender',
+  filters: [
+    { text: 'Male', value: 'male' },
+    { text: 'Female', value: 'female' },
+  ],
+  width: '20%',
+}, {
+  title: 'Email',
+  dataIndex: 'email',
+},{
+  title: 'image',
+  dataIndex: 'picture',
+  render: picture =><img src= {picture.thumbnail}/>,
+  width: '10%',
+},
+];
+
+class App extends React.Component {
+  state = {
+    data: [],
+    pagination: {},
+    loading: false,
+  };
+
+  componentDidMount() {
+    this.fetch();
+  }
+
+  handleTableChange = (pagination, filters, sorter) => {
+    const pager = { ...this.state.pagination };
+    pager.current = pagination.current;
+    this.setState({
+      pagination: pager,
+    });
+    this.fetch({
+      results: pagination.pageSize,
+      page: pagination.current,
+      sortField: sorter.field,
+      sortOrder: sorter.order,
+      ...filters,
+    });
+  }
+
+  fetch = (params = {}) => {
+    console.log('params:', params);
+    this.setState({ loading: true });
+    reqwest({
+      url: 'https://randomuser.me/api',
+      method: 'get',
+      data: {
+        results: 10,
+        ...params,
+      },
+      type: 'json',
+    }).then((data) => {
+      const pagination = { ...this.state.pagination };
+      // Read total count from server
+      // pagination.total = data.totalCount;
+      pagination.total = 200;
+      this.setState({
+        loading: false,
+        data: data.results,
+        pagination,
+      });
+    });
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Table
+        columns={columns}
+        rowKey={record => record.login.uuid}
+        dataSource={this.state.data}
+        pagination={this.state.pagination}
+        loading={this.state.loading}
+        onChange={this.handleTableChange}
+        scroll={{ y: 240 }}
+      ></Table>
     );
   }
 }
