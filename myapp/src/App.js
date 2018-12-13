0cc302432825e2f4bc7687bb7e9c37d150255793
Johnny <szhczhao@cn.ibm.com>
2018-12-13 12:59:45 +0800

    add table

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -3,93 +3,74 @@ import logo from './logo.svg';
 import './App.css';
 import { Table } from 'antd';
 import reqwest from 'reqwest';
+import TablePri from './TablePri.js'
+import {
+  Layout, Menu, Breadcrumb, Icon,
+} from 'antd';
 
-const columns = [{
-  title: 'Name',
-  dataIndex: 'name',
-  sorter: true,
-  render: name => `${name.first} ${name.last}`,
-  width: '20%',
-}, {
-  title: 'Gender',
-  dataIndex: 'gender',
-  filters: [
-    { text: 'Male', value: 'male' },
-    { text: 'Female', value: 'female' },
-  ],
-  width: '20%',
-}, {
-  title: 'Email',
-  dataIndex: 'email',
-},{
-  title: 'image',
-  dataIndex: 'picture',
-  render: picture =><img src= {picture.thumbnail}/>,
-  width: '10%',
-},
-];
+const { SubMenu } = Menu;
+const { Header, Content, Sider } = Layout;
+
 
 class App extends React.Component {
-  state = {
-    data: [],
-    pagination: {},
-    loading: false,
-  };
-
-  componentDidMount() {
-    this.fetch();
-  }
-
-  handleTableChange = (pagination, filters, sorter) => {
-    const pager = { ...this.state.pagination };
-    pager.current = pagination.current;
-    this.setState({
-      pagination: pager,
-    });
-    this.fetch({
-      results: pagination.pageSize,
-      page: pagination.current,
-      sortField: sorter.field,
-      sortOrder: sorter.order,
-      ...filters,
-    });
-  }
-
-  fetch = (params = {}) => {
-    console.log('params:', params);
-    this.setState({ loading: true });
-    reqwest({
-      url: 'https://randomuser.me/api',
-      method: 'get',
-      data: {
-        results: 10,
-        ...params,
-      },
-      type: 'json',
-    }).then((data) => {
-      const pagination = { ...this.state.pagination };
-      // Read total count from server
-      // pagination.total = data.totalCount;
-      pagination.total = 200;
-      this.setState({
-        loading: false,
-        data: data.results,
-        pagination,
-      });
-    });
-  }
+ 
 
   render() {
     return (
-      <Table
-        columns={columns}
-        rowKey={record => record.login.uuid}
-        dataSource={this.state.data}
-        pagination={this.state.pagination}
-        loading={this.state.loading}
-        onChange={this.handleTableChange}
-        scroll={{ y: 240 }}
-      ></Table>
+      <Layout>
+    <Header className="header">
+      <div className="logo" />
+      <Menu
+        theme="dark"
+        mode="horizontal"
+        defaultSelectedKeys={['2']}
+        style={{ lineHeight: '64px' }}
+      >
+        <Menu.Item key="1">nav 1</Menu.Item>
+        <Menu.Item key="2">nav 2</Menu.Item>
+        <Menu.Item key="3">nav 3</Menu.Item>
+      </Menu>
+    </Header>
+    <Layout>
+      <Sider width={200} style={{ background: '#fff' }}>
+        <Menu
+          mode="inline"
+          defaultSelectedKeys={['1']}
+          defaultOpenKeys={['sub1']}
+          style={{ height: '100%', borderRight: 0 }}
+        >
+          <SubMenu key="sub1" title={<span><Icon type="user" />subnav 1</span>}>
+            <Menu.Item key="1">option1</Menu.Item>
+            <Menu.Item key="2">option2</Menu.Item>
+            <Menu.Item key="3">option3</Menu.Item>
+            <Menu.Item key="4">option4</Menu.Item>
+          </SubMenu>
+          <SubMenu key="sub2" title={<span><Icon type="laptop" />subnav 2</span>}>
+            <Menu.Item key="5">option5</Menu.Item>
+            <Menu.Item key="6">option6</Menu.Item>
+            <Menu.Item key="7">option7</Menu.Item>
+            <Menu.Item key="8">option8</Menu.Item>
+          </SubMenu>
+          <SubMenu key="sub3" title={<span><Icon type="notification" />subnav 3</span>}>
+            <Menu.Item key="9">option9</Menu.Item>
+            <Menu.Item key="10">option10</Menu.Item>
+            <Menu.Item key="11">option11</Menu.Item>
+            <Menu.Item key="12">option12</Menu.Item>
+          </SubMenu>
+        </Menu>
+      </Sider>
+      <Layout style={{ padding: '0 24px 24px' }}>
+        <Breadcrumb style={{ margin: '16px 0' }}>
+          <Breadcrumb.Item>Home</Breadcrumb.Item>
+          <Breadcrumb.Item>List</Breadcrumb.Item>
+          <Breadcrumb.Item>App</Breadcrumb.Item>
+        </Breadcrumb>
+        <TablePri style={{
+          background: '#fff', padding: 24, margin: 0, minHeight: 280,
+        }}></TablePri>
+      </Layout>
+    </Layout>
+  </Layout>
     );
   }
 }
